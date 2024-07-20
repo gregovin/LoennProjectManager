57f3eeb9161be1bc445fda14444bfd4a499e2a96
Gregory Hess <gregory.w.hess@gmail.com>
2024-07-20 07:47:01 -0700

    More Formatting

--- Loenn/project_manager_tools/versionNotif.lua
+++ Loenn/project_manager_tools/versionNotif.lua
@@ -3,10 +3,10 @@ local warningGenerator = mods.requireFromPlugin("libraries.warningGenerator")
 local meta = require("meta")
 
 local script = warningGenerator.makeWarning(
-    {("loenn version %s is not supported by this version of loenn project manager"):format(meta.version),
-    "Ensure Loenn Project manager and loenn are up to date(and restart loenn).",
-    "If that fails, ping @gregovin on the discord"}
+    { ("loenn version %s is not supported by this version of loenn project manager"):format(meta.version),
+        "Ensure Loenn Project manager and loenn are up to date(and restart loenn).",
+        "If that fails, ping @gregovin on the discord" }
 )
-script.layer="project",
+script.layer = "project"
 script.displayName = "Version Error"
 return script
