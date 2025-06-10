f16591ab999f1276af922856b2a8698ca06151b2
Gregory Hess <gregory.w.hess@gmail.com>
2025-06-09 19:20:20 -0700

    Include marker icons

--- Loenn/project_manager_tools/configureOverworld.lua
+++ Loenn/project_manager_tools/configureOverworld.lua
@@ -6,6 +6,9 @@ local projectLoader = mods.requireFromPlugin("libraries.projectLoader")
 local history = require("history")
 local logging = require("logging")
 local pUtils = mods.requireFromPlugin("libraries.projectUtils")
+local fileSystem = require("utils.filesystem")
+local fileLocations = require("file_locations")
+local modsDir = fileSystem.joinpath(fileLocations.getCelesteDir(), "Mods")
 
 local script = {
     name = "configureOverworldDetails",
@@ -19,7 +22,8 @@ local script = {
         starFogColor = "020915",
         starStreamColors = { "000000", "9228e2", "30ffff" },
         starBeltColors1 = { "53f3dd", "53c9f3" },
-        starBeltColors2 = { "ab6ffa", "fa70ea" }
+        starBeltColors2 = { "ab6ffa", "fa70ea" },
+        markerIcon = "marker/runBackpack",
     },
     tooltips = {
         showSnow         = "Weather or not to show the snow on the overworld",
@@ -31,6 +35,7 @@ local script = {
         "The colors of the small stars rotating around the moon. They are dispatched in 2 \"belts\" that are slightly misaligned between each other.",
         starBeltColors2  =
         "The colors of the small stars rotating around the moon. They are dispatched in 2 \"belts\" that are slightly misaligned between each other.",
+        markerIcon = "Which map marker icon to use on the overworld when this map is selected.",
     },
     fieldInformation = {
         showSnow = { fieldType = "boolean" },
@@ -51,6 +56,10 @@ local script = {
         },
         starBeltColors2 = {
             fieldType = "loennProjectManager.expandableColorList"
+        },
+        markerIcon = {
+            fieldType="string",
+            options = {"marker/Fall","marker/runBackpack", "marker/runNoBackpack"}
         }
     },
     fieldOrder = { "fogColors", "showSnow", "starFogColor", "starStreamColors", "starBeltColors1", "starBeltColors2" }
@@ -61,6 +70,16 @@ function script.prerun()
         projectLoader.assertStateValid(projectDetails)
         if not projectLoader.cacheValid then
             projectLoader.loadMetadataDetails(projectDetails)
+            script.fieldInformation.markerIcon.options={"marker/Fall","marker/runBackpack", "marker/runNoBackpack"}
+            local searchpath = fileSystem.joinpath(modsDir, projectDetails.name,"Graphics","Atlases","Mountain","marker",projectDetails.username,projectDetails.campaign)
+            if fileSystem.isDirectory(searchpath) then
+                for _,v in ipairs(pUtils.list_dir(searchpath)) do
+                    local true_n = string.match(fileSystem.filename(v), "^%D*")
+                    if not pUtils.contains(script.fieldInformation.markerIcon.options, true_n) then
+                        table.insert(script.fieldInformation.markerIcon.options, "marker/" .. true_n)
+                    end
+                end
+            end
         end
         script.parameters.fogColors = metadataHandler.getNestedValueOrDefault({ "Mountain", "FogColors" })
         script.parameters.showSnow = metadataHandler.getNestedValueOrDefault({ "Mountain", "ShowSnow" })
@@ -68,6 +87,8 @@ function script.prerun()
         script.parameters.starStreamColors = metadataHandler.getNestedValueOrDefault({ "Mountain", "StarStreamColors" })
         script.parameters.starBeltColors1 = metadataHandler.getNestedValueOrDefault({ "Mountain", "StarBeltColors1" })
         script.parameters.starBeltColors2 = metadataHandler.getNestedValueOrDefault({ "Mountain", "StarBeltColors2" })
+        script.parameters.markerIcon = metadataHandler.getNestedValueOrDefault({ "Mountain","MarkerTexture"})
+        
     elseif not projectDetails.name then
         error("Cannot find metadata because no project is selected!", 2)
     elseif not projectDetails.username then
@@ -97,6 +118,7 @@ function script.run(args)
     end
     metadataHandler.setNestedIfNotDefault({ "Mountain", "StarBeltColors1" }, beltC1)
     metadataHandler.setNestedIfNotDefault({ "Mountain", "StarBeltColors2" }, beltC2)
+    metadataHandler.setNestedIfNotDefault({ "Mountain","MarkerTexture"}, args.markerIcon)
     local dataAfter = utils.deepcopy(metadataHandler.loadedData)
     local forward = function()
         metadataHandler.loadedData = dataAfter
